94ea4ab6203a4c286fa32df31f5fd5ade714967e
jeantim <jean.vouillamoz@gmail.com>
2019-07-11 09:14:23 +0200

    merge index.php and style.css

---
+++ www/Index.php
@@ -0,0 +1,314 @@
+<?php 
+//require_once('classPromenades');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Annuaire de Ballades</title>
+    <meta http-equiv="Content-Type" content="txt/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+
+
+
+</head>
+
+<body>
+    <header>
+        <div class="container-fluid">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                <img class="pics" src="assets/baby-feet-icon-4.png" width="50" height="50" alt="feet-icon">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+
+                    <span class="navbar-toggler-icon"></span>
+
+                </button>
+
+                <div class="collapse navbar-collapse offset-5" id="navbarTogglerDemo02">
+
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
+
+                        <li class="nav-item active">
+
+                            <a class="nav-link " href="#">Index </a>
+
+                        </li>
+
+                        <li class="nav-item">
+
+                            <a class="nav-link" href="#">Ajouter</a>
+
+                        </li>
+
+                        <li class="nav-item">
+
+                            <a class="nav-link" href="#">Modifier</a>
+
+                        </li>
+
+                    </ul>
+
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+
+                </div>
+
+            </nav>
+
+        </div>
+
+    </header>
+
+    <section>
+    </section>
+<!---------------------------------------LES IMAGES---------------------------------------->
+    <section class="container-fluid">
+        <br>
+        <!----ROW-->
+        <div class="row no-gutters  justify-content-around">
+
+
+
+
+            <div class="col-xl-2 col-xs-6">
+                <div class="container ">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"> </a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                   
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"> </a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                   
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"> </a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                   
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"> </a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                   
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"></a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                    
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"> </a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                   
+                </div>
+            </div>
+        </div>
+        <!----ROW-->
+        <br>
+        <!----ROW-->
+        <div class="row  no-gutters flex-lg-wrap">
+
+                <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"></a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                    
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"> </a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                   
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"> </a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                   
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"></a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                    
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"></a>
+                        <div class="centered txt">Ma super Ballade</div>
+                        <div class="top-left txt">Suisse</div>
+                        <div class="top-right txt">Appenzeler</div>
+                        <div class="centered-bottom txt">par Jean-Louis</div>
+                        <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                                découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+                    
+                </div>
+            </div>
+            <div class="col-xl-2 col-xs-6">
+                <div class="container lastpics">
+                    <a href="afficherInfo.html"> <img class="pics" src="assets/B1.jpg" alt="Snow" style="width:100%;"></a>
+                    <div class="centered txt">Ma super Ballade</div>
+                    <div class="top-left txt">Suisse</div>
+                    <div class="top-right txt">Appenzeler</div>
+                    <div class="centered-bottom txt">par Jean-Louis</div>
+                    <div class="descript ">Entre les vallons de l'Appenzeler, la promenade va vous emmener
+                        découvrire les plus beau lieu de l'Appenzeler. Faune et fleurs de montagne au rendez-vous.</div>
+
+                </div>
+            </div>
+        </div>
+        <!----ROW-->
+
+        <br>
+    </section>
+
+
+
+
+
+
+
+
+
+
+    <footer>
+        <!--Jean-->
+
+        <div class="container-fluid ">
+
+            <!-- Grid row -->
+            <div class="row justify-content-around align-items-center">
+
+                <!-- Grid column -->
+                <div class="col-md-2 ">
+
+                    <!-- Content -->
+                    <h5>WebMasters</h5>
+                    <p>Projet réalisé par Obada et Jean en classe digitale 2019 à Réalise</p>
+
+                </div>
+
+                <!-- Social buttons -->
+                <ul class="list-unstyled list-inline txt-center">
+                    <li class="list-inline-item">
+                        <a class="btn-floating btn-fb mx-1" href="https://www.facebook.com/"><img
+                            class="icon"       src="assets/icon-FB.png" alt="" width="50">
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="btn-floating btn-tw mx-1" href="https://twitter.com">
+                            <img class="icon" src="assets/icon-Twitter.png" alt="" width="50">
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a class="btn-floating btn-gplus mx-1" href="https://www.youtube.com/?hl=FR">
+                            <img class="icon" src="assets/icon-YT.png" alt="" width="50">
+                        </a>
+                    </li>
+
+                </ul>
+                <!-- Social buttons -->
+
+                <!-- Call to action -->
+                <ul class="list-unstyled">
+                    <li>
+                        <h5 class="mb-1">Besoin d'un site Web ? </h5>
+                    </li>
+                    <li>
+                        <a href="#!" class="btn btn-secondary btn-rounded">Demandez votre devis!</a>
+                    </li>
+                </ul>
+                <!-- Call to action -->
+
+    </footer>
+    <!-- Footer -->
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
