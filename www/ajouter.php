a1dd21c50d2af43ec22698644a55aedd7410768c
Obada ALSAID MHIA <obadah.sayed@gmail.com>
2019-07-11 11:32:26 +0200

    added ajouter.php

---
+++ www/ajouter.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+
+<html>  
+
+    <header>
+
+         
+
+    </header>
+
+    <body>
+
+        <form action="process-create.php" method="post">
+            Titre: <br>
+
+            <input type="text" name="titre"> <br>
+
+            Pays: <br>
+
+            <input type="text" name="pays"> <br>
+            
+            Ville: <br>
+
+            <input type="text" name="ville"> <br>
+
+            Code Postal: <br>
+
+            <input type="number" name="codePostale"> <br>
+
+            Lieu de départ: <br>
+
+            <input type="text" name="depart"> <br>
+
+            Lieu d'arrivée: <br>
+
+            <input type="text" name="arrivee"> <br>
+
+            Auteur: <br>
+
+            <input type="text" name="auteur"> <br>
+
+            Descripition: <br>
+
+            <textarea rows="10" cols="50" type="text" name="description" maxlength="300"> </textarea> <br>
+
+
+            <input type="submit">
+        
+        </form>
+
+    </body>
+
+</html>
